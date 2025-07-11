52b6e9045155d85db2356dd4ce342c9937f8725c
jonas <jbs2larry@gmail.com>
2025-07-11 18:59:03 +0200

    add supabase client

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,24 @@
+import { type NextRequest } from "next/server";
 import createMiddleware from "next-intl/middleware";
+import { updateSession } from "@/supabase/middleware";
 import { routing } from "./i18n/routing";
 
-export default createMiddleware(routing);
+const intlMiddleware = createMiddleware(routing);
+
+export async function middleware(request: NextRequest) {
+  const intlResponse = intlMiddleware(request);
+
+  // Ensure we have a response object to pass along
+
+  if (intlResponse && intlResponse.status !== 200) {
+    return intlResponse;
+  }
+
+  return await updateSession(request, intlResponse);
+}
 
 export const config = {
-  // Match all pathnames except for
-  // - … if they start with `/api`, `/trpc`, `/_next` or `/_vercel`
-  // - … the ones containing a dot (e.g. `favicon.ico`)
-  matcher: "/((?!api|trpc|_next|_vercel|.*\\..*).*)",
+  matcher: [
+    "/((?!api|trpc|_next|_vercel|.*\\.(?:svg|png|jpg|jpeg|gif|webp|ico)$).*)",
+  ],
 };
