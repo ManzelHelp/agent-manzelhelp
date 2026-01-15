3ea623e67f27ef4c9d6d2b6560aa908501e562a3
admin2gobeldi <gobeldicom@gmail.com>
2026-01-15 11:55:05 +0100

    Update localization settings to make French the default language, reorder supported locales, and enable automatic locale detection based on browser settings.

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -2,8 +2,14 @@ import { defineRouting } from "next-intl/routing";
 
 export const routing = defineRouting({
   // A list of all locales that are supported
-  locales: ["en", "de", "ar", "fr"],
+  // Order matters: French is first as it's the default
+  locales: ["fr", "ar", "de", "en"],
 
   // Used when no locale matches
-  defaultLocale: "en",
+  // French is now the default language
+  defaultLocale: "fr",
+  
+  // Enable locale detection based on browser/device language
+  // This uses the Accept-Language header automatically
+  localeDetection: true,
 });
